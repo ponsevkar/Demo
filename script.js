9f878d4071614a9427795b710d9c551e6f591909
ponsevkar <46707603+ponsevkar@users.noreply.github.com>
2019-01-23 16:05:31 +0100

    A lot

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let newsArticles = [
+{
+  title: "News article title 1",
+subtitle: "Subtitle1",
+description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+},
+{
+  title: "News article title 2",
+  subtitle: "Subtitle2",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+},
+{
+  title: "News article title 3",
+  subtitle: "Subtitle4",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+}
+]
+
+let articlesDiv = document.querySelector(".news")
+
+newsArticles.forEach(article => {
+  articlesDiv.innerHTML += `
+  <div class ="article">
+    <h2>${article.title}</h2>
+    <p>${article.description}</p>
+  
+   </div>
+    `
+})
