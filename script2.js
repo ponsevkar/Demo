32b3647c5db122dfff320f6c094e2dd7cc0f37fa
ponsevkar <46707603+ponsevkar@users.noreply.github.com>
2019-01-30 09:25:15 +0100

    Name update

--- script2.js
+++ script2.js
@@ -46,3 +46,23 @@
     fetch(urlleft)
       .then(response => response.json())
       .then(receivedNewsLeft)
+
+
+      let urltotalleft = "https://newsapi.org/v2/everything?q=trade%20war%20AND%20trump&apiKey=789530644cd74ad69e6e406a4b9e30b0&from=2019-01-30"
+
+      let receivedNewsCounterLeft = (newsData) => {
+
+          newsData.article.length((article) => {
+
+            document.querySelector(".newsCounterLeft").innerHTML +=
+              `<div class="topmentions">
+                    <h5>${totalResults}</h5>
+              </div>`
+
+          })
+        }
+
+
+        fetch(urltotalleft)
+          .then(response => response.json())
+          .then(receivedNewsCounterLeft)
